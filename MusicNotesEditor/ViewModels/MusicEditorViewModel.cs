0f1d6b52e3addd5a560f948823ab34393b8fc783
Firax666 <kimseatis@gmail.com>
2025-10-29 18:31:25 +0100

    load initial data and fix measures

--- MusicNotesEditor/ViewModels/MusicEditorViewModel.cs
+++ MusicNotesEditor/ViewModels/MusicEditorViewModel.cs
@@ -1,20 +1,32 @@
-﻿using System.Windows.Navigation;
-using System.Xml.Linq;
-using Manufaktura.Controls.Audio;
+﻿using Manufaktura.Controls.Audio;
 using Manufaktura.Controls.Desktop.Audio;
 using Manufaktura.Controls.Model;
+using Manufaktura.Controls.Model.Collections;
+using Manufaktura.Controls.Model.Events;
+using Manufaktura.Controls.Model.Rules;
 using Manufaktura.Controls.Parser;
+using Manufaktura.Controls.Primitives;
+using Manufaktura.Controls.WPF;
 using Manufaktura.Music.Model;
 using Manufaktura.Music.Model.MajorAndMinor;
-using MusicNotesEditor.Views;
-using static System.Formats.Asn1.AsnWriter;
-
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Xml;
 using System.Xml.Linq;
+
 namespace MusicNotesEditor.ViewModels
 {
     class MusicEditorViewModel : ViewModel
     {
+        private const int MAX_NUMBER_OF_STAVES = 10;
+        private const int MAX_INITIAL_STAVE_LINES = 3;
+
         public RhythmicDuration? CurrentNote = null;
+        public double NoteViewerContentWidth;
+        public double NoteViewerContentHeight;
         private ScorePlayer player;
         private Score data;
         public Score Data
@@ -22,21 +34,90 @@ namespace MusicNotesEditor.ViewModels
             get { return data; }
             set { data = value; OnPropertyChanged(() => Data); }
         }
-        public void LoadTestData()
+        public void LoadInitialData()
         {
-            var score = Score.CreateOneStaffScore(Clef.Treble, new MajorScale(Step.C, false));
-            score.FirstStaff.Elements.Add(new Note(Pitch.C5, RhythmicDuration.Quarter));
-            score.FirstStaff.Elements.Add(new Note(Pitch.B4, RhythmicDuration.Quarter));
-            score.FirstStaff.Elements.Add(new Note(Pitch.C5, RhythmicDuration.Half));
-            score.FirstStaff.Elements.Add(new Note(Pitch.C5, RhythmicDuration.Half));
-            score.FirstStaff.Elements.Add(new Barline()); 
+            LoadInitialData(1);
+        }
+
+        public void LoadInitialData(int numberOfParts)
+        {
+            if(numberOfParts < 1)
+            {
+                throw new InvalidOperationException("Can't create new score with less than 1 staff.");
+            }
+            else if (numberOfParts > MAX_NUMBER_OF_STAVES)
+            {
+                throw new InvalidOperationException($"Can't create new score with more than {MAX_NUMBER_OF_STAVES} staff.");
+            }
+
+            var score = new Score();
+         
+            for (int i = 0; i < numberOfParts; i++)
+            {
+                score.AddStaff(Clef.Treble, TimeSignature.CommonTime, Step.C, MajorAndMinorScaleFlags.MajorSharp);
+                score.Staves[i].MeasureAddingRule = Staff.MeasureAddingRuleEnum.AddMeasuresManually;
+                score.Staves[i].AddTimeSignature(TimeSignatureType.Numbers, 4, 4);
+                score.Staves[i].AddBarline(BarlineStyle.None);
+                var part = new Part(score.Staves[i]) { PartId = $"P{i+1}" };
+                score.Parts.Add(part);
+            }
+        
             Data = score;
         }
+
+        public void LoadInitialTemplate()
+        {
+            LoadInitialTemplate(1);
+        }
+
+        public void LoadInitialTemplate(int numberOfParts)
+        {
+
+            for (int i = 0; i < numberOfParts; i++)
+            {
+
+                int staffLinesCount = 1;
+                // Fill up stave line 
+                while (true)
+                {
+                    Data.Staves[i].Add(new Rest(RhythmicDuration.Whole));
+                    Data.Staves[i].AddBarline(BarlineStyle.Regular);
+                    var barlines = Data.Staves[i].Elements.OfType<Barline>();
+                    double lastBarlineXPosition = barlines.Last().ActualRenderedBounds.SE.X;
+                    
+                    // until it reaches width
+                    if (lastBarlineXPosition > NoteViewerContentWidth)
+                    {
+                        RemoveLastN(Data.Staves[i].Elements, 2);
+                        if(staffLinesCount < MAX_INITIAL_STAVE_LINES)
+                            AddNewLine(Data.Staves[i]);
+
+                     
+                        staffLinesCount++;
+                    }
+
+                    if (staffLinesCount > MAX_INITIAL_STAVE_LINES)
+                    {
+                        RemoveLastN(Data.Staves[i].Elements, 1);
+                        FixMeasures(Data.Staves[i]);
+                        break;
+                    }
+                }
+
+            }
+        }
+
 
         public void LoadData(string filepath)
         {
             var parser = new MusicXmlParser();
             var score = parser.Parse(XDocument.Load(filepath));
+
+            foreach (var staff in score.Staves)
+            {
+                FixMeasures(staff);
+            }
+
             Data = score;
         }
 
@@ -45,5 +126,107 @@ namespace MusicNotesEditor.ViewModels
             player = player = new MidiTaskScorePlayer(Data);
             player.Play();
         }
+
+
+        public static void RemoveLastN<T>(ItemManagingCollection<T> collection, int n)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (n <= 0) return;
+
+            int count = collection.Count;
+            int removeCount = Math.Min(n, count);
+
+            // remove from the end
+            for (int i = 0; i < removeCount; i++)
+            {
+                collection.RemoveAt(collection.Count - 1);
+            }
+        }
+
+        public static void AddNewLine(Staff staff)
+        {
+            FixMeasures(staff);
+
+            var lineBreak = new PrintSuggestion() { IsSystemBreak = true };
+            staff.Add(lineBreak);
+        }
+
+
+
+
+        public static void FixMeasures(Staff staff)
+        {
+            if (staff == null)
+                throw new ArgumentNullException(nameof(staff));
+
+            staff.Measures.Clear();
+
+            var currentMeasure = new Measure(staff, null) { Number = 1 };
+            staff.Measures.Add(currentMeasure);
+
+            foreach (var e in staff.Elements)
+            {
+                // Find which system this element belongs to
+                var system = GetSystemForElement(staff, e);
+                if (system != null)
+                    currentMeasure.System = system;
+
+                currentMeasure.Elements.Add(e);
+                var property = typeof(MusicalSymbol).GetProperty("Measure",
+                    System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public);
+                property.SetValue(e, currentMeasure);
+
+
+                if (e is Barline)
+                {
+                    currentMeasure = new Measure(staff, GetSystemForElement(staff, e))
+                    {
+                        Number = staff.Measures.Count + 1
+                    };
+                    staff.Measures.Add(currentMeasure);
+                }
+            }
+
+            // Remove trailing empty measure
+            if (staff.Measures.Last().Elements.Count == 0 && staff.Measures.Count > 1)
+                staff.Measures.RemoveAt(staff.Measures.Count - 1);
+
+            RaiseStaffInvalidated(staff);
+        }
+
+        protected static StaffSystem GetSystemForElement(Staff staff, MusicalSymbol element)
+        {
+            if (staff.Score == null || staff.Score.Systems == null || staff.Score.Systems.Count == 0)
+                return null;
+
+            // Count all system breaks that happened before this element
+            int breakCount = staff.Elements
+                .TakeWhile(e => e != element)
+                .Count(e => e is PrintSuggestion ps && ps.IsSystemBreak);
+
+            // If we have more breaks than systems, clamp
+            breakCount = Math.Min(breakCount, staff.Score.Systems.Count - 1);
+
+            // Return the corresponding system
+            return staff.Score.Systems[breakCount];
+        }
+
+
+        private static void RaiseStaffInvalidated(Staff staff)
+        {
+            var evtField = typeof(Staff).GetField("StaffInvalidated",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+            var del = (MulticastDelegate)evtField?.GetValue(staff);
+            if (del == null) return;
+
+            del.DynamicInvoke(staff, new InvalidateEventArgs<Staff>(staff));
+        }
+
+        public void FuckingDebug()
+        {
+
+        }
+
     }
 }
--- MusicNotesEditor/Views/MusicEditorPage.xaml.cs
+++ MusicNotesEditor/Views/MusicEditorPage.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Navigation;
-using MusicNotesEditor.ViewModels;
 
 namespace MusicNotesEditor.Views
 {
@@ -31,18 +30,28 @@ namespace MusicNotesEditor.Views
             GenerateNoteButtons();
 
             DataContext = viewModel;
+
             try
             {
                 if (string.IsNullOrEmpty(filepath))
-                    viewModel.LoadTestData();
+                {
+                    viewModel.LoadInitialData();
+                    /* 
+                     * Part of initializing data has to be moved to first render
+                     * as it needs rendering bounds of barlines to put system breaks correctly
+                    */
+                    CompositionTarget.Rendering += InitializeDataOnFirstRender;
+                }
+
                 else
                     viewModel.LoadData(filepath);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                // Should handle different exception and actually go back to main menu
-                // with error message
-                viewModel.LoadTestData();
+                // Should navigate back to main menu with displaying error, should catch different exceptions
+                Console.WriteLine("Loading Error:");
+                Console.WriteLine(ex);
+                viewModel.LoadInitialData();
             }
 
             mainGrid.SizeChanged += MainGrid_SizeChanged;
@@ -52,7 +61,7 @@ namespace MusicNotesEditor.Views
             noteViewer.MouseEnter += Canvas_MouseEnter;
             noteViewer.MouseLeave += Canvas_MouseLeave;
             noteViewer.MouseMove += Canvas_MouseMove;
-
+            
             noteIndicator = new TextBlock
             {
                 FontSize = 26,
@@ -62,6 +71,28 @@ namespace MusicNotesEditor.Views
 
         }
 
+        private void InitializeDataOnFirstRender(object? sender, EventArgs e)
+        {
+            viewModel.NoteViewerContentWidth = NoteViewerContentWidth();
+            viewModel.NoteViewerContentHeight = NoteViewerContentHeight();
+            CompositionTarget.Rendering -= InitializeDataOnFirstRender;
+            viewModel.LoadInitialTemplate();
+        }
+
+
+        private double NoteViewerContentWidth()
+        {
+            return (noteViewer.Width - 
+                (noteViewer.Padding.Right + noteViewer.Padding.Left))
+                / noteViewer.ZoomFactor;
+        }
+
+        private double NoteViewerContentHeight()
+        {
+            return (noteViewer.Height -
+                (noteViewer.Padding.Top + noteViewer.Padding.Bottom))
+                / noteViewer.ZoomFactor;
+        }
 
         private void GenerateNoteButtons()
         {
@@ -205,8 +236,51 @@ namespace MusicNotesEditor.Views
         }
 
 
+        private void NoteViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Function for printing debug information about note viewer
+        // Runs when it's clicked. It will be messy. Delete after finishing note viewer.
         private void NoteViewer_Debug(object sender, MouseButtonEventArgs e)
         {
+
+            var staves = viewModel.Data.Staves;
+            var systems = viewModel.Data.Systems;
+            var parts = viewModel.Data.Parts;
+            var pages = viewModel.Data.Pages;
+
+            Console.WriteLine($"Staves: {staves.Count}");
+            Console.WriteLine($"Systems: {systems.Count}");
+            Console.WriteLine($"Parts: {parts.Count}");
+            Console.WriteLine($"Pages: {pages.Count}");
+
+            Console.WriteLine("XD");
+
+            var barlines = viewModel.Data.FirstStaff.Elements.OfType<Barline>();
+            double lastBarlineYPosition = barlines.Last().ActualRenderedBounds.SE.X;
+
+            Console.WriteLine($"\nLast Barline: {lastBarlineYPosition}");
+            Console.WriteLine($"Bounds: {mainCanvas.ActualWidth}");
+
+            barlines = viewModel.Data.FirstStaff.Elements.OfType<Barline>();
+            lastBarlineYPosition = barlines.Last().ActualRenderedBounds.SE.X;
+
+            Console.WriteLine($"\nLast Barline2: {lastBarlineYPosition}");
+            Console.WriteLine($"Bounds2: {noteViewer.Width}");
+            Console.WriteLine($"Bounds3: {NoteViewerContentWidth}");
+
+
+            foreach (var staff in viewModel.Data.Staves)
+            {
+                foreach (var measure in staff.Measures)
+                {
+                    Console.WriteLine(measure.ToString());
+                }
+            }
+
+
             if (noteViewer.SelectedElement != null)
             {
                 Console.WriteLine($"Selected element: {noteViewer.SelectedElement} Location: {noteViewer.SelectedElement.RenderedWidth} Type:{noteViewer.SelectedElement.GetType()}");
@@ -218,23 +292,24 @@ namespace MusicNotesEditor.Views
             }
             Console.WriteLine("\nAll elements\n:");
 
-            var staves = noteViewer.ScoreSource.Staves;
+            //var staves = noteViewer.ScoreSource.Staves;
 
-            for (int i = 0; i < staves.Count; i++)
-            {
-                var elements = staves[i].Elements;
-                for (int j = 0; j < elements.Count; j++)
-                {
-                    Console.WriteLine($"\tStave: {i + 1} Element: {j + 1}. {elements[j]} Location: {elements[j].ActualRenderedBounds}");
-                }
-            }
+            //for (int i = 0; i < staves.Count; i++)
+            //{
+            //    var elements = staves[i].Elements;
+            //    for (int j = 0; j < elements.Count; j++)
+            //    {
+            //        Console.WriteLine($"\tStave: {i + 1} Element: {j + 1}. {elements[j]} Location: {elements[j].ActualRenderedBounds}");
+            //        var element = elements[j] as Note;
+            //        if(element != null)
+            //        {
+            //            //element.Pitch = Pitch.A4;
+            //        }
+            //    }
+            //}
 
             Console.WriteLine("\n\n");
         }
 
-        private void NoteViewer_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
